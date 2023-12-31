7a146e1caa441c8d2b7e82c62ddbd970c5452dce
Sourav163 <souravroutray2016@gmail.com>
2024-01-01 00:26:00 +0530

    Thread Methods

--- ThreadMethods.java
+++ ThreadMethods.java
@@ -6,22 +6,45 @@ class MyThread0 extends Thread {
     }
 
     public void run() {
-        while(true)
+        int i = 0;
+        while(i < 100) {
             System.out.println("I am " + this.getName());
+            i++;
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class MyThread00 extends Thread {
+    public MyThread00(String name) {
+        super(name);
+    }
+
+    public void run() {
+        int i = 0;
+        while(i < 80) {
+            System.out.println("I am " + this.getName());
+            i++;
+        }
     }
 }
 
 public class ThreadMethods {
     public static void main(String[] args) {
         MyThread0 th0 = new MyThread0("Sourav");
-        MyThread0 th1 = new MyThread0("Routray");
+        MyThread00 th00 = new MyThread00("Routray");
         th0.start();
-        try {
-            th0.join();
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-        th1.start();
+//        try {
+//            th0.join();
+//        }
+//        catch(Exception e) {
+//            System.out.println(e);
+//        }
+        th00.start();
     }
 }
